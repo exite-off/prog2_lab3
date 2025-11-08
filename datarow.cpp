9b8f9661c946b9f39ab779b9b4b7530cecf90294
exite-off <vfedin868@gmail.com>
2025-11-08 18:08:58 +0200

    More readable code, fixed CMakeLists.txt

--- datarow.cpp
+++ datarow.cpp
@@ -1,6 +1,7 @@
 ﻿#include "datarow.h"
 
 #include <string>
+#include <format>
 
 DataRow::DataRow() {
     fields[0] = 0;
@@ -8,17 +9,17 @@ DataRow::DataRow() {
 }
 
 int DataRow::get(int index) const {
-    std::lock_guard<std::mutex> lock(mtx[index]);
+    std::lock_guard lock(mtx[index]);
     return fields[index];
 }
 
 void DataRow::set(int index, int value) {
-    std::lock_guard<std::mutex> lock(mtx[index]);
+    std::lock_guard lock(mtx[index]);
     fields[index] = value;
 }
 
 DataRow::operator std::string() const {
-    std::lock_guard<std::mutex> lock0(mtx[0]);
-    std::lock_guard<std::mutex> lock1(mtx[1]);
-    return "[" + std::to_string(fields[0]) + ", " + std::to_string(fields[1]) + "]";
+    std::lock_guard lock0(mtx[0]);
+    std::lock_guard lock1(mtx[1]);
+    return std::format("[{}, {}]", fields[0], fields[1]);
 }
