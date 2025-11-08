d1908d64c1e2b6d866e95329e61b0bdee23f00cd
exite-off <vfedin868@gmail.com>
2025-11-08 15:58:10 +0200

    Initial thoughts on structure, to do with templates

--- datarow.cpp
+++ datarow.cpp
@@ -1,1 +1,24 @@
 ﻿#include "datarow.h"
+
+#include <string>
+
+DataRow::DataRow() {
+    fields[0] = 0;
+    fields[1] = 0;
+}
+
+int DataRow::get(int index) const {
+    std::lock_guard<std::mutex> lock(mtx[index]);
+    return fields[index];
+}
+
+void DataRow::set(int index, int value) {
+    std::lock_guard<std::mutex> lock(mtx[index]);
+    fields[index] = value;
+}
+
+DataRow::operator std::string() const {
+    std::lock_guard<std::mutex> lock0(mtx[0]);
+    std::lock_guard<std::mutex> lock1(mtx[1]);
+    return "[" + std::to_string(fields[0]) + ", " + std::to_string(fields[1]) + "]";
+}
--- datarow.h
+++ datarow.h
@@ -1,7 +1,17 @@
 ﻿#pragma once
 
-class DataRow {
+#include <mutex>
+#include <string>
 
+class DataRow {
+private:
+    int fields[2]{};
+    mutable std::mutex mtx[2];
+public:
+    DataRow();
+    int get(int index) const;
+    void set(int index, int value);
+    operator std::string() const;
 };
 
 
